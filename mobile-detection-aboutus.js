32d50748710b6a5054677f5c8fcf4474105a9cd7
projectjade <projectjade2017@gmail.com>
2017-05-14 20:45:13 +0800

    Create mobile-detection-aboutus.js

---
+++ mobile-detection-aboutus.js
@@ -0,0 +1,15 @@
+//mobile detection
+      var sniffer=new Object();
+      var agentNow=navigator.userAgent.toLowerCase();
+      sniffer.android=(agentNow.search("android")>=0);
+      sniffer.series60=(agentNow.search("series60")>=0);
+      sniffer.iphone=(agentNow.search("iphone")>=0);
+      sniffer.blackberry=(agentNow.search("blackberry")>=0);
+      sniffer.windowsce=(agentNow.search("windowsce")>=0);
+      for(var mobile in sniffer)
+      {
+        if(sniffer[mobile])
+        {
+          window.location = "mobile-aboutus.html";
+        } 
+      }
