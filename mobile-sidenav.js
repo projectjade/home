8b4fb66cf15fb182abc0abebf5b47a82932f4586
projectjade <projectjade2017@gmail.com>
2017-05-14 19:50:24 +0800

    Update mobile-sidenav.js

--- mobile-sidenav.js
+++ mobile-sidenav.js
@@ -1,7 +1,7 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
       function openNav() {
-        document.getElementById("mySidenav").style.width = "280px";
-        document.getElementById("main").style.marginLeft = "280px";
+        document.getElementById("mySidenav").style.width = "290px";
+        document.getElementById("main").style.marginLeft = "290px";
         document.body.style.backgroundColor = "rgba(0,0,0,0.3)";
       }
 
